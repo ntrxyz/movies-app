a67b389911ca5051417fce60688faf7e89ba5de1
ntrxyz <ntri.knp@gmail.com>
2025-02-17 20:48:02 +0530

    Created Movies and Review classes for endpoints creation

--- movies/src/main/java/dev/nupur/movies/Movie.java
+++ movies/src/main/java/dev/nupur/movies/Movie.java
@@ -5,14 +5,15 @@ import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
 @Document(collection = "movies")//framework knows that the class represents each doc in movies collection
-@Data
-@AllArgsConstructor
+@Data //takes care of getters and setters and toString methods
+@AllArgsConstructor //constructor that takes all private fields as argument
 public class Movie {
-    @Id
+    @Id //this will let framework know that this property should be treated as the unique identifier for each movie in db
     private ObjectId id;
     private String imdbId;
     private String title;
@@ -21,5 +22,7 @@ public class Movie {
     private String poster;
     private List<String> genres;
     private List<String> backdrops;
+    @DocumentReference//cause db to store only the IDs of the reviews and review will be in separate collection
+    private List<Review> reviewIds;
 
 }
